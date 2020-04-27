ca84e13ff326933fd8ae01b361c145e832e23297
Jim <43685375+jim-lub@users.noreply.github.com>
2020-04-27 13:13:37 +0200

    initial commit

---
+++ src/views/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>GameMaker</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/views/components/CodeEditor/CodeEditor.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CodeEditor = () => {
+  return (
+    <div>
+      <pre>
+        <div></div>
+      </pre>
+      <textarea />
+    </div>
+  )
+}
+
+export default CodeEditor;
